b495842918954e1e1dfad9a218257ba521402f6a
Ronan <dineen1@gmail.com>
2024-08-05 12:26:32 +0200

    add assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental property',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
