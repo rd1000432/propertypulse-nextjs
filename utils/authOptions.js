55f1924df03a47529f1f99604d6c536f7b159d81
Ronan <dineen1@gmail.com>
2024-08-13 10:59:17 +0200

    fix signIn function typo in authOptions

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
+
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
@@ -17,17 +20,33 @@ export const authOptions = {
   ],
   callbacks: {
     // Invoked on successful sign in
-    async singIn({ profile }) {
+    async signIn({ profile }) {
       // 1. Connect to the database
+      await connectDB();
       // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3. if not, create user
+      if (!userExists) {
+        // Truncate username if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // 4. Return true to allow sign in
+      return true;
     },
     // Session callback function that modifies the session object
     async session({ session }) {
       //1. Get the user from the database
+      const user = await User.findOne({ email: session.user.email });
       //2. Assign user id from the session
+      session.user.id = user._id.toString();
       //3. Return session
+      return session;
     },
   },
 };
