a0998bbbf40e60de8f1201f4070f345c57dde8c2
Ronan <dineen1@gmail.com>
2024-08-13 12:10:43 +0200

    profileImage display

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,8 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
   const { data: session } = useSession();
-  console.log(session);
+  // add question mark in case it is null - wont throw any errors (optional chaining)
+  const profileImage = session?.user?.image;
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -166,7 +167,9 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage || profileDefault}
+                      width={40}
+                      height={40}
                       alt=""
                     />
                   </button>
