6daffc58eda2561f6cb45c4c5360dbc8ef5c0a2a
Ronan <dineen1@gmail.com>
2024-08-16 10:34:43 +0200

    add property action and cloudinary image upload

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,15 +1,29 @@
 'use server';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache'; // Once we submit update cache and listings
+import { redirect } from 'next/navigation';
+import cloudinary from '@/config/cloudinary';
 
 async function addProperty(formData) {
+  await connectDB();
+  // get session user
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error('User ID is required');
+  }
+
+  const { userId } = sessionUser;
+
   // Access all values from amenities and images
   const amenities = formData.getAll('amenities');
   //create an array of images and filter out any blanks
-  const images = formData
-    .getAll('images')
-    .filter((image) => image.name !== '')
-    .map((image) => image.name);
+  const images = formData.getAll('images').filter((image) => image.name !== '');
 
   const propertyData = {
+    owner: userId,
     type: formData.get('type'),
     name: formData.get('name'),
     description: formData.get('description'),
@@ -33,10 +47,38 @@ async function addProperty(formData) {
       email: formData.get('seller_info.email'),
       phone: formData.get('seller_info.phone'),
     },
-    images,
   };
 
-  console.log(propertyData);
+  const imageUrls = [];
+
+  for (const imageFile of images) {
+    const imageBuffer = await imageFile.arrayBuffer();
+    const imageArray = Array.from(new Uint8Array(imageBuffer));
+    const imageData = Buffer.from(imageArray);
+
+    // Convert to base64
+    const imageBase64 = imageData.toString('base64');
+
+    // Make request to cloudinary
+    const result = await cloudinary.uploader.upload(
+      `data:image/png;base64,${imageBase64}`,
+      {
+        folder: 'propertypulse',
+      }
+    );
+
+    imageUrls.push(result.secure_url);
+  }
+
+  propertyData.images = imageUrls;
+
+  // submit to database
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath('/', 'layout');
+
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
